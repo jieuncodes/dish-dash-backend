16c65a70f9893c446a43ba1ce0b0560c1e607350
youredith <110220308+jieuncodes@users.noreply.github.com>
2023-10-24 00:09:09 +0900

    mapped types allows me to generate diff versions of data types

--- src/restaurants/restaurants.resolver.ts
+++ src/restaurants/restaurants.resolver.ts
@@ -1,18 +1,34 @@
 import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
-import { Restaurant } from './entities/restaurant.entity';
-import { create } from 'domain';
 import { CreateRestaurantDto } from './dtos/create-restaurant.dto';
-import { RestaurantService } from './restaurants.servive';
+import { UpdateRestaurantDto } from './dtos/update-restaurant.dto';
+import { Restaurant } from './entities/restaurant.entity';
+import { RestaurantService } from './restaurants.service';
 
 @Resolver((of) => Restaurant)
-export class RestaurantsResolver {
+export class RestaurantResolver {
   constructor(private readonly restaurantService: RestaurantService) {}
   @Query((returns) => [Restaurant])
   restaurants(): Promise<Restaurant[]> {
     return this.restaurantService.getAll();
   }
   @Mutation((returns) => Boolean)
-  createRestaurant(@Args() createRestaurantDto: CreateRestaurantDto): boolean {
+  async createRestaurant(
+    @Args('input') createRestaurantDto: CreateRestaurantDto,
+  ): Promise<boolean> {
+    console.log(createRestaurantDto);
+    try {
+      await this.restaurantService.createRestaurant(createRestaurantDto);
+      return true;
+    } catch (e) {
+      console.log(e);
+      return false;
+    }
+  }
+
+  @Mutation((returns) => Boolean)
+  async updateRestaurant(
+    @Args('input') updateRestaurantDto: UpdateRestaurantDto,
+  ) {
     return true;
   }
 }
