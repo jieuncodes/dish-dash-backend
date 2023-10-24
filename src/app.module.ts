8af1c1362c582aecfaa7ad6a0b61d1f0365e2086
youredith <110220308+jieuncodes@users.noreply.github.com>
2023-10-24 21:24:35 +0900

    User resolver setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,9 @@ import { RestaurantsModule } from './restaurants/restaurants.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
 import { Restaurant } from './restaurants/entities/restaurant.entity';
+import { UsersModule } from './users/users.module';
+import { CommonModule } from './common/common.module';
+import { User } from './users/entities/user.entity';
 
 @Module({
   imports: [
@@ -35,13 +38,14 @@ import { Restaurant } from './restaurants/entities/restaurant.entity';
       database: process.env.DATABASE,
       synchronize: process.env.NODE_ENV !== 'prod',
       logging: true,
-      entities: [Restaurant],
+      entities: [User],
     }),
     GraphQLModule.forRoot({
       driver: ApolloDriver,
       autoSchemaFile: true,
     }),
-    RestaurantsModule,
+    UsersModule,
+    CommonModule,
   ],
   controllers: [],
   providers: [],
