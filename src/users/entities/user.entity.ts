471f5734c291ce8073af284520b142d4f9289c16
youredith <110220308+jieuncodes@users.noreply.github.com>
2023-10-24 21:43:42 +0900

    feat: create account mutation

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,16 +1,22 @@
+import { Field, InputType, ObjectType } from '@nestjs/graphql';
 import { CoreEntity } from 'src/common/entities/core.entity';
 import { Column, Entity } from 'typeorm';
 
 type UserRole = 'client' | 'owner' | 'delivery';
 
+@InputType({ isAbstract: true })
+@ObjectType()
 @Entity()
 export class User extends CoreEntity {
-  @Column()
+  @Column((type) => String)
+  @Field((type) => String)
   email: string;
 
-  @Column()
+  @Column((type) => String)
+  @Field((type) => String)
   password: string;
 
-  @Column()
+  @Column((type) => String)
+  @Field((type) => String)
   role: UserRole;
 }
--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -1,7 +1,11 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { User } from './entities/user.entity';
 import { Injectable } from '@nestjs/common';
 import { UsersService } from './users.service';
+import {
+  CreateAccountInputs,
+  CreateAccountOutput,
+} from './dtos/create-account.dto';
 
 @Resolver((of) => User)
 export class UsersResolver {
@@ -11,4 +15,7 @@ export class UsersResolver {
   hi() {
     return true;
   }
+
+  @Mutation((returns) => CreateAccountOutput)
+  createAccount(@Args('input') createAccountInput: CreateAccountInputs) {}
 }
