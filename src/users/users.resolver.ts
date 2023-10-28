150eec002e92c8f054cc4dfa4226806ee9d7db3f
youredith <110220308+jieuncodes@users.noreply.github.com>
2023-10-28 13:08:29 +0900

    feat: userProfile Mutation

--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -14,6 +14,7 @@ import { LoginInput, LoginOutput } from './dtos/login.dto';
 import { CreateAccountInput } from './dtos/create-account.dto';
 import { AuthGuard } from 'src/auth/auth.guard';
 import { AuthUser } from 'src/auth/auth-user.decorator';
+import { UserProfileInput, UserProfileOutput } from './dtos/user-profile.dto';
 
 @Resolver((of) => User)
 export class UsersResolver {
@@ -58,5 +59,27 @@ export class UsersResolver {
   @UseGuards(AuthGuard)
   me(@AuthUser() authUser: User) {
     return authUser;
+  }
+
+  @UseGuards(AuthGuard)
+  @Query((returns) => UserProfileOutput)
+  async userProfile(
+    @Args('id') UserProfileInput: UserProfileInput,
+  ): Promise<UserProfileOutput> {
+    try {
+      const user = await this.usersService.findById(UserProfileInput.userId);
+      if (!user) {
+        throw Error();
+      }
+      return {
+        ok: true,
+        user,
+      };
+    } catch (e) {
+      return {
+        ok: false,
+        error: 'User Not Found',
+      };
+    }
   }
 }
